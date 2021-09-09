ac1255e29e78ed75e60b8c1a324d49856542180e
sepehr <sepehrn@stud.ntnu.no>
2021-09-09 14:02:02 +0200

    removed dependency

--- test.py
+++ test.py
@@ -8,18 +8,17 @@ import datetime
 client = MongoClient(
     "mongodb+srv://NORBIT:OFNvXhOKBipn9ieq@cluster0.nc1za.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
-# # Create collection (collection = Table)
-# db = client.gettingStarted
-# # create row
-# test = db.test
+# Create collection (collection = Table)
+db = client.gettingStarted
+# create row
+test = db.test
 
-# # JSON formatted for input row
-# testDocument = {
-#     "name": {"First": "NORBIT", "Last": "Kundestyrt"}
-# }
-# # Insert row
-# test.insert_one(testDocument)
+# JSON formatted for input row
+testDocument = {
+    "name": {"First": "NORBIT", "Last": "Kundestyrt"}
+}
+# Insert row
+test.insert_one(testDocument)
 
-# # Issue the serverStatus command and print the results
-# serverStatusResult = db.command("serverStatus")
-# TESTING
+# Issue the serverStatus command and print the results
+serverStatusResult = db.command("serverStatus")

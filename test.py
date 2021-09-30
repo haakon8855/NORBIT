6e331c1d4e2a9898c5bf2b067cfddb7df9dfd127
Helene Elling <56873088+heleneelling@users.noreply.github.com>
2021-09-30 14:55:16 +0200

    Setup (#2)
    * Set up connection with database, set up python enviroment
    
    * Added a way to add tables and rows to database
    
    * commented out for not populating
    
    * Made dev branch and added gitignore
    
    * Added class for calling api
    
    * Fixed gitignore
    
    * testing if prettifier works
    
    * removed dependency
    
    * Moved environment variable to env.py
    
    Removed api-key and secret from code.
    
    * Added env to gitignore
    
    * Removed env
    
    * Data filtering for moving to MongoDB
    
    Co-authored-by: sepehr <sepehrn@stud.ntnu.no>
    Co-authored-by: Håkon Anders <haakon8855@users.noreply.github.com>
    Co-authored-by: Sepehr <43407185+sepehrn0107@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+from pprint import pprint
+import datetime
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+# Username and password for cloud:
+#Username: NORBIT
+# PasswordOFNvXhOKBipn9ieq
+client = MongoClient(
+    "mongodb+srv://NORBIT:OFNvXhOKBipn9ieq@cluster0.nc1za.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+# Create collection (collection = Table)
+db = client.gettingStarted
+# create row
+test = db.test
+
+# JSON formatted for input row
+testDocument = {
+    "name": {"First": "NORBIT", "Last": "Kundestyrt"}
+}
+# Insert row
+test.insert_one(testDocument)
+
+# Issue the serverStatus command and print the results
+serverStatusResult = db.command("serverStatus")

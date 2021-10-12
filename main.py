b5e99fd610d25e2f6faa412d467c868d95f93bc8
Emil Ekornrød <emil.ekornrod@gmail.com>
2021-10-12 13:01:53 +0200

    create endpoint for updating callibrationData

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from flask import Flask, Response, jsonify, request
 import pymongo
+from pymongo import collection
+import move_data
 from flask_restful import abort
 from pymongo.errors import DuplicateKeyError
 from env import *
 
 # Configure Flask & Flask-PyMongo:
 app = Flask(__name__)
-client = pymongo.MongoClient(DB_URI, port=DB_PORT, tls=True, tlsAllowInvalidHostnames=True,
-                             tlsCAFile=DB_CA_FILE, username=DB_USERNAME, password=DB_PASSWORD)
+CLIENT = None
+DB = None
+LAST_UPDATE = None
 BeaconValues = {}
-db = client.testdb
+
+def init():
+    global CLIENT, DB, LAST_UPDATE
+    CLIENT = pymongo.MongoClient(DB_URI, port=DB_PORT, tls=True, tlsAllowInvalidHostnames=True,
+                             tlsCAFile=DB_CA_FILE, username=DB_USERNAME, password=DB_PASSWORD)
+    DB = CLIENT.testdb
+    LAST_UPDATE = move_data.get_last_updated(CLIENT, "callibrationData")
+
 
 def abort_beaconId_not_found(beaconId):
     if beaconId not in BeaconValues:
@@ -36,7 +46,7 @@ def resource_not_found(e):
 def post_td(beaconId: int) -> Response:
     new_value = request.get_json()
     BeaconValues[beaconId] = new_value
-    db.td.insert_one(new_value)
+    DB.td.insert_one(new_value)
     return jsonify(message="success"), 201
 
 
@@ -45,13 +55,14 @@ def delete_td(beaconId):
     abort_beaconId_not_found(beaconId)
     del BeaconValues[beaconId]
 
-    db.td.delete_many({"_id": beaconId})
+    DB.td.delete_many({"_id": beaconId})
     return "", 204
 
+
 @app.route('/td/<int:deviceId>/', methods=['GET'])
-def get_td(deviceId: int) -> Response:
+def get_td(deviceId: int):
     try:
-        td = db.td.find({"_id": int(deviceId)})[0]
+        td = DB.td.find({"_id": int(deviceId)})[0]
     except IndexError:
         print("index error")
         return "", 404
@@ -59,8 +70,16 @@ def get_td(deviceId: int) -> Response:
     BeaconValues[deviceId] = td
     return jsonify([td])
 
+
+@app.route('/update')
+def update() -> Response:
+    global LAST_UPDATE
+    updated_at = move_data.update_callibration(CLIENT , 1, 41, LAST_UPDATE)
+    LAST_UPDATE = updated_at if updated_at != 0 else LAST_UPDATE
+    return "sucess", 200
+
+
 if __name__ == "__main__":
-    for x in db.td.find({}):
-        BeaconValues[x["_id"]] = x
+    init()
     app.run(debug=True)
-    client.close()
+    CLIENT.close()

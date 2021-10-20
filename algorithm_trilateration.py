e11d0737b192c76f644f568256cc1ee4a649b237
Håkon <haakon8855@users.noreply.github.com>
2021-10-20 13:06:23 +0200

    Fix function name and remove comments

--- algorithm_trilateration.py
+++ algorithm_trilateration.py
@@ -18,9 +18,6 @@ def algorithm_trilateration(db_client: pymongo.MongoClient):
         # db_client.testdb.callibrationData.find({'timestamp': 1633680792}))
         # db_client.testdb.callibrationData.find({'timestamp': 1633673904}))
         db_client.testdb.callibrationData.find({'timestamp': 1633673758}))
-    # {'timestamp': {
-    #     '$gt': 1633573592
-    # }})
     data = data[[
         "rssi", "gatewayId", "gatewayLat", "gatewayLng", "positionLat",
         "positionLng", "timestamp"
@@ -83,7 +80,7 @@ def prediction(data):
         gateways = np.array(data[["gatewayLat", "gatewayLng"]])
 
         # pred_lat, pred_lng = trilat(distances_to_gateways, gateways)
-        pred_lat, pred_lng = trilat2(distances_to_gateways, gateways)
+        pred_lat, pred_lng = trilat(distances_to_gateways, gateways)
         if pred_lat is None or pred_lng is None:
             return prediction(data.iloc[0:2, :])
         return {
@@ -99,72 +96,6 @@ def rssi2dist(rssis):
     measured_power = -69  # Factory calibrated?
     N = 1.14  # Environmental factor
     return 10**((measured_power - rssis) / (10 * N))
-
-
-def trilat(distances_to_gateways, gateways):
-    """
-    Takes as an argument a line from the input file (see script docstring for
-    expected format) and returns the trilaterated GPS coordinates of the
-    sample.
-    """
-    threshold = -0.004
-    earthR = 6371
-    DistA = float(distances_to_gateways[0]) / 1000
-    DistB = float(distances_to_gateways[1]) / 1000
-    DistC = float(distances_to_gateways[2]) / 1000
-    LatA = float(gateways[0][0])
-    LonA = float(gateways[0][1])
-    LatB = float(gateways[1][0])
-    LonB = float(gateways[1][1])
-    LatC = float(gateways[2][0])
-    LonC = float(gateways[2][1])
-
-    #   1. Convert Lat/Long to radians
-    #   2. Convert Lat/Long(radians) to ECEF
-    xA = earthR * (math.cos(math.radians(LatA)) * math.cos(math.radians(LonA)))
-    yA = earthR * (math.cos(math.radians(LatA)) * math.sin(math.radians(LonA)))
-    zA = earthR * (math.sin(math.radians(LatA)))
-
-    xB = earthR * (math.cos(math.radians(LatB)) * math.cos(math.radians(LonB)))
-    yB = earthR * (math.cos(math.radians(LatB)) * math.sin(math.radians(LonB)))
-    zB = earthR * (math.sin(math.radians(LatB)))
-
-    xC = earthR * (math.cos(math.radians(LatC)) * math.cos(math.radians(LonC)))
-    yC = earthR * (math.cos(math.radians(LatC)) * math.sin(math.radians(LonC)))
-    zC = earthR * (math.sin(math.radians(LatC)))
-
-    P1 = np.array([xA, yA, zA])
-    P2 = np.array([xB, yB, zB])
-    P3 = np.array([xC, yC, zC])
-
-    ex = (P2 - P1) / (np.linalg.norm(P2 - P1))
-    i = np.dot(ex, P3 - P1)
-    ey = (P3 - P1 - i * ex) / (np.linalg.norm(P3 - P1 - i * ex))
-    ez = np.cross(ex, ey)
-    d = np.linalg.norm(P2 - P1)
-    j = np.dot(ey, P3 - P1)
-
-    x = (pow(DistA, 2) - pow(DistB, 2) + pow(d, 2)) / (2 * d)
-    y = ((pow(DistA, 2) - pow(DistC, 2) + pow(i, 2) + pow(j, 2)) /
-         (2 * j)) - ((i / j) * x)
-
-    z2 = pow(DistA, 2) - pow(x, 2) - pow(y, 2)
-    print("z2:", z2)
-    if threshold < z2 < 0:
-        z2 = 0
-    elif z2 < threshold:
-        return None, None
-    z = np.sqrt(z2)
-
-    #triPt is an array with ECEF x,y,z of trilateration point
-    triPt = P1 + x * ex + y * ey + z * ez
-
-    #convert back to lat/long from ECEF
-    #convert to degrees
-    lat = math.degrees(math.asin(triPt[2] / earthR))
-    lon = math.degrees(math.atan2(triPt[1], triPt[0]))
-
-    return lat, lon
 
 
 def gps_solve(distances_to_station, stations_coordinates):
@@ -185,7 +116,7 @@ def gps_solve(distances_to_station, stations_coordinates):
                     method='Nelder-Mead').x
 
 
-def trilat2(distances_to_gateways, gateways):
+def trilat(distances_to_gateways, gateways):
     xA, yA, colA, rowA = utm.from_latlon(float(gateways[0][0]),
                                          float(gateways[0][1]))
     xB, yB, colB, rowB = utm.from_latlon(float(gateways[1][0]),
